df53b751e556871d71a374cf024fa3049059f106
Michael Sumner <mdsumner@gmail.com>
2018-03-07 14:36:09 +1100

    now fix the added regression ...

--- R/ibble-sf.R
+++ R/ibble-sf.R
@@ -1,22 +1,23 @@
 types <- c(POINT = 1L, MULTIPOINT = 2L, LINESTRING = 3L, MULTILINESTRING = 4L, POLYGON = 5L, MULTIPOLYGON = 6L, Polygons = 7L, Lines = 8L, Points = 9L, MultiPoints = 10L)
 
 ibble.POINT <- function(x, ...) cbind(nrow = 1, ncol = length(unclass(x)), type = 1L)
+ibble.MULTITHING <- function(x, ...) {dm <- dim(unclass(x)); cbind(nrow = dm[1L], ncol = dm[2], type = 2L)}
 ibble.MULTIPOINT <- function(x, ...) {dm <- dim(unclass(x)); cbind(nrow = rep(1L, dm[1]), ncol = dm[2], type = 2L)}
 ibble.LINESTRING  <- function(x, ...) {dm <- dim(unclass(x)); cbind(nrow = dm[1], ncol = dm[2], type = 3L)}
 
 
 ibble.MULTILINESTRING <- function(x, ...) {
-  out <- do.call(rbind, lapply(unclass(x), ibble.MULTIPOINT))
+  out <- do.call(rbind, lapply(unclass(x), ibble.MULTITHING))
   out[, "type"] <- 4L
   out
 }
 ibble.POLYGON <- function(x, ...) {
-  out <- do.call(rbind, lapply(unclass(x), ibble.MULTIPOINT))
+  out <- do.call(rbind, lapply(unclass(x), ibble.MULTITHING))
   out[, "type"] <- 5L
   out
 }
 ibble.POLYPART <- function(x, subobject = 1L, ...) {
- out <- do.call(rbind, lapply(x, ibble.MULTIPOINT))
+ out <- do.call(rbind, lapply(x, ibble.MULTITHING))
  cbind(out, subobject = subobject)
 }
 ibble.MULTIPOLYGON <- function(x, ...) {
