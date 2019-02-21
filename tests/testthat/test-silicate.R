f95c6522685707f012a5577c477c8e4d9d4ed1d1
Michael Sumner <mdsumner@gmail.com>
2019-02-21 06:25:51 +0000

    test subobject

--- tests/testthat/test-silicate.R
+++ tests/testthat/test-silicate.R
@@ -2,6 +2,6 @@ context("test-silicate")
 
 
 test_that("PATH works", {
-  expect_s3_class(gibble(point), "tbl_df") %>% expect_named(c("nrow", "ncol", "type", "object"))
-  expect_s3_class(gibble(mpoint), "tbl_df") %>% expect_named(c("nrow", "ncol", "type", "object"))
+  expect_s3_class(gibble(point), "tbl_df") %>% expect_named(c("nrow", "ncol", "type", "subobject", "object"))
+  expect_s3_class(gibble(mpoint), "tbl_df") %>% expect_named(c("nrow", "ncol", "type", "subobject", "object"))
 })
